8921fd86d3ef8ff25de78eb40c15de9ae497a391
Lucas Soto <lucaslsoto95@gmail.com>
2020-12-18 11:59:05 -0300

    Add persistent data paths creation script

---
+++ make-data-paths.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Create mysql user
+
+sudo userdel mysql
+sudo useradd -u 999 mysql
+
+# Create and make mysql owner of the data folder
+
+sudo mkdir -p /data/mysql
+sudo chown -R mysql:mysql /data/mysql
+
+# Create www-data user
+
+sudo userdel www-data
+sudo useradd -u 82 www-data
+
+# Create and make www-data owner of the data folder
+
+sudo mkdir -p /data/html
+sudo chown -R www-data:www-data /data/html
