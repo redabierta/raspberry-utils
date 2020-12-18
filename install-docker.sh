17cf27d8f0d1418bf841260b0a863479e7b991cc
Lucas Soto <lucaslsoto95@gmail.com>
2020-12-18 18:55:58 +0000

    [FIX] Docker installation scripts

--- install-docker.sh
+++ install-docker.sh
@@ -1,33 +1,11 @@
 #!/usr/bin/env bash
 
-# Install Docker Engine
+sudo sh get-docker.sh
 
-sudo apt-get update
+sudo usermod -aG docker ${USER}
 
-sudo apt-get install \
-    apt-transport-https \
-    ca-certificates \
-    curl \
-    gnupg-agent \
-    software-properties-common
+sudo apt-get install libffi-dev libssl-dev
+sudo apt install python3-dev
+sudo apt-get install -y python3 python3-pip
 
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo apt-key add -
-
-sudo apt-key fingerprint 0EBFCD88
-
-sudo add-apt-repository \
-   "deb [arch=arm64] https://download.docker.com/linux/debian \
-   $(lsb_release -cs) \
-   stable"
-
-sudo apt-get update
-
-sudo apt-get install docker-ce docker-ce-cli containerd.io
-
-# Install Docker Compose
-
-sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
-
-sudo chmod +x /usr/local/bin/docker-compose
-
-sudo ln -s /usr/local/bin/docker-compose /usr/bin/docker-compose
+sudo pip3 install docker-compose
